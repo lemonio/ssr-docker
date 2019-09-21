2b7673cec72fb2acb8bcaa1d063c584df346f293
lemonio <450369812@qq.com>
2019-09-21 14:27:18 +0000

    first commit

--- ssr-bbr.sh
+++ ssr-bbr.sh
@@ -1,5 +1,5 @@
 yum install -y yum-utils device-mapper-persistent-data lvm2
-sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 yum clean all
 yum makecache
 yum -y install docker-ce
